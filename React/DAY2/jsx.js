c72968ea8998c55f542f1f2cf49a56422dc4ba9d
SeshuYaswanth <seshuyaswanthreddy01@gmail.com>
2025-03-21 23:51:57 +0530

    new file

---
+++ React/DAY2/jsx.js
@@ -0,0 +1,7 @@
+import { createElement } from "react";
+import ReactDOM from "react-dom";
+
+const heading = <h1>Hello World</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
